d0b926c2645af1f343df11f4ae4f5705d0a400f3
bjanssens <bjanssens@inuits.eu>
2017-03-31 16:39:14 +0200

    Client.pp: Do not define $name
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- manifests/client.pp
+++ manifests/client.pp
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class ipa::client(
-  $name = '',      # what define was called with...
   $hostname = $::hostname,
   $domain = $::domain,
   $realm = '',      # defaults to upcase($domain)
